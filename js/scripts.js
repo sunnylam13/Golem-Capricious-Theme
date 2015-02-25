1ef1a617c8b1192a85aa2f0e8e84d793e408045c
Sunny Lam <sunny.lam@gmail.com>
2015-02-24 19:46:26 -0500

    Tried to setup the portfolio filter javascript and haven't had any luck so far getting it to work.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,10 @@ jQuery(document).ready(function($) {
 //////////////////////////////////////////////////
 // FUNCTIONS
 
+////////////////////////////////////////////
+/////     FACEBOOK COMMENTS FOR POSTS
+///////////////////////////////////////////////
+
 (function(d, s, id) {
 	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) return;
@@ -15,8 +19,14 @@ jQuery(document).ready(function($) {
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+// END FACEBOOK COMMENTS FOR POSTS
+
 //////////////////////////////////////////////////
 // EXECUTION CODE
+
+////////////////////////////////////////////
+/////     IMAGE SLIDER FOR HOME WITH CAROUSEL
+///////////////////////////////////////////////
 
 $('.bxslider').bxSlider({
 	controls: true,
@@ -24,6 +34,12 @@ $('.bxslider').bxSlider({
 	autoControls: true,
 	slideWidth: 702
 });
+
+// END IMAGE SLIDER FOR HOME WITH CAROUSEL
+
+////////////////////////////////////////////
+/////     LATEST WORKS IMAGE SLIDER
+///////////////////////////////////////////////
 
 $('.bxsliderLW').bxSlider({
   minSlides: 3,
@@ -37,6 +53,52 @@ $('.bxsliderLW').bxSlider({
   // nextText: 'Onward →',
   // prevText: '← Go back'
 });
+
+// END LATEST WORKS IMAGE SLIDER
+
+////////////////////////////////////////////
+/////     PORTFOLIO FILTER
+///////////////////////////////////////////////
+
+// fancybox
+$(".fancybox").fancybox({
+    modal: true, // disable regular nav and close buttons
+    // add buttons helper (requires buttons helper js and css files)
+    helpers: {
+        buttons: {}
+    } 
+});
+
+// filter selector
+$(".filter").on("click", function () {
+    var $this = $(this);
+    // if we click the active tab, do nothing
+    if ( !$this.hasClass("active") ) {
+        $(".filter").removeClass("active");
+        $this.addClass("active"); // set the active tab
+        // get the data-rel value from selected tab and set as filter
+        var $filter = $this.data("rel"); 
+        // if we select view all, return to initial settings and show all
+        $filter == 'all' ? 
+            $(".fancybox")
+            .attr("data-fancybox-group", "gallery")
+            .not(":visible")
+            .fadeIn() 
+        : // otherwise
+            $(".fancybox")
+            .fadeOut(0)
+            .filter(function () {
+                // set data-filter value as the data-rel value of selected tab
+                return $(this).data("filter") == $filter; 
+            })
+            // set data-fancybox-group and show filtered elements
+            .attr("data-fancybox-group", $filter)
+            .fadeIn(1000); 
+    } // if
+}); // on
+
+
+// END PORTFOLIO FILTER
 
 //////////////////////////////////////////////////
 
