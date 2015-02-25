8cefc540f0fa92d562c2edfd2dd28b559209d60f
Sunny Lam <sunny.lam@gmail.com>
2015-02-24 20:26:23 -0500

    Used jQuery show and hide functions to achieve a filtering effect with a duration of "fast" to provide some transition effects. Can't seem to figure out how to change the colour of the nav button however.

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,13 @@ jQuery(document).ready(function($) {
 
 // END FACEBOOK COMMENTS FOR POSTS
 
+function checkIfActiveClass () {
+  if (!$this.hasClass('active')) {
+    $('.filter').removeClass('active');
+    $this.addClass('active');
+  }
+}
+
 //////////////////////////////////////////////////
 // EXECUTION CODE
 
@@ -60,42 +67,101 @@ $('.bxsliderLW').bxSlider({
 /////     PORTFOLIO FILTER
 ///////////////////////////////////////////////
 
-// fancybox
-$(".fancybox").fancybox({
-    modal: true, // disable regular nav and close buttons
-    // add buttons helper (requires buttons helper js and css files)
-    helpers: {
-        buttons: {}
-    } 
+// so far haven't gotten the fancybox method to work
+
+// // fancybox
+// $(".fancybox").fancybox({
+//     modal: true, // disable regular nav and close buttons
+//     // add buttons helper (requires buttons helper js and css files)
+//     helpers: {
+//         buttons: {}
+//     } 
+// });
+
+// // filter selector
+// $(".filter").on("click", function () {
+//     var $this = $(this);
+//     // if we click the active tab, do nothing
+//     if ( !$this.hasClass("active") ) {
+//         $(".filter").removeClass("active");
+//         $this.addClass("active"); // set the active tab
+//         // get the data-rel value from selected tab and set as filter
+//         var $filter = $this.data("rel"); 
+//         // if we select view all, return to initial settings and show all
+//         $filter == 'all' ? 
+//             $(".fancybox")
+//             .attr("data-fancybox-group", "gallery")
+//             .not(":visible")
+//             .fadeIn() 
+//         : // otherwise
+//             $(".fancybox")
+//             .fadeOut(0)
+//             .filter(function () {
+//                 // set data-filter value as the data-rel value of selected tab
+//                 return $(this).data("filter") == $filter; 
+//             })
+//             // set data-fancybox-group and show filtered elements
+//             .attr("data-fancybox-group", $filter)
+//             .fadeIn(1000); 
+//     } // if
+// }); // on
+
+$("a.all").on('click', function() {
+  // if (!$this.hasClass('active')) {
+  //   $('.filter').removeClass('active');
+  //   $this.addClass('active');
+  // }
+  // $('a.filter').removeClass('active');
+  // $this.addClass('active');
+  $('.portWeb').show("fast");
+  $('.portPhoto').show("fast");
+  $('.portGraphic').show("fast");
+  $('.portVideo').show("fast");
 });
 
-// filter selector
-$(".filter").on("click", function () {
-    var $this = $(this);
-    // if we click the active tab, do nothing
-    if ( !$this.hasClass("active") ) {
-        $(".filter").removeClass("active");
-        $this.addClass("active"); // set the active tab
-        // get the data-rel value from selected tab and set as filter
-        var $filter = $this.data("rel"); 
-        // if we select view all, return to initial settings and show all
-        $filter == 'all' ? 
-            $(".fancybox")
-            .attr("data-fancybox-group", "gallery")
-            .not(":visible")
-            .fadeIn() 
-        : // otherwise
-            $(".fancybox")
-            .fadeOut(0)
-            .filter(function () {
-                // set data-filter value as the data-rel value of selected tab
-                return $(this).data("filter") == $filter; 
-            })
-            // set data-fancybox-group and show filtered elements
-            .attr("data-fancybox-group", $filter)
-            .fadeIn(1000); 
-    } // if
-}); // on
+$("a.web").on('click', function() {
+  // if (!$this.hasClass('active')) {
+  //   $('.filter').removeClass('active');
+  //   $this.addClass('active');
+  // }
+  $('.portWeb').show("fast");
+  $('.portPhoto').hide("fast");
+  $('.portGraphic').hide("fast");
+  $('.portVideo').hide("fast");
+});
+
+$("a.photo").on('click', function() {
+  // if (!$this.hasClass('active')) {
+  //   $('.filter').removeClass('active');
+  //   $this.addClass('active');
+  // }
+  $('.portWeb').hide("fast");
+  $('.portPhoto').show("fast");
+  $('.portGraphic').hide("fast");
+  $('.portVideo').hide("fast");
+});
+
+$("a.graphic").on('click', function() {
+  // if (!$this.hasClass('active')) {
+  //   $('.filter').removeClass('active');
+  //   $this.addClass('active');
+  // }
+  $('.portWeb').hide("fast");
+  $('.portPhoto').hide("fast");
+  $('.portGraphic').show("fast");
+  $('.portVideo').hide("fast");
+});
+
+$("a.video").on('click', function() {
+  // if (!$this.hasClass('active')) {
+  //   $('.filter').removeClass('active');
+  //   $this.addClass('active');
+  // }
+  $('.portWeb').hide("fast");
+  $('.portPhoto').hide("fast");
+  $('.portGraphic').hide("fast");
+  $('.portVideo').show("fast");
+});
 
 
 // END PORTFOLIO FILTER
